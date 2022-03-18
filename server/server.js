f9e1ca11edb3f4b25f52d8bed963851eeb1c430e
Arthur Baronov <abaronov1234@gmail.com>
2022-03-17 18:11:46 -0600

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '574af1c192074233a73e94c97819ebce',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+//Serving files using middleware (express.static)
+app.use(express.static(path.join(__dirname, "../public")))
+
+//Serving files using endpoints (app.get())
+// app.get("/", function(req, res){
+//     res.sendFile(path.join(__dirname, "../public"));
+// });
+
+const port = process.env.PORT || 4005;
+
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+});
